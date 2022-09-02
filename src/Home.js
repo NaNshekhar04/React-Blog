5bca4cce452a3ba4eb933f687137a9af4bf57fdd
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-09-02 20:12:44 +0530

    Added

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body:'lorem ipsum....', author: 'mario', id:1},
-        {title: 'Welcome party', body:'lorem ipsum....', author: 'shekhar', id:2},
-        {title: 'Web dev top tips', body:'lorem ipsum....', author: 'vineet', id:3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setisPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
+        .then (res =>{
+                if(!res.ok){
+                    throw Error('Unable to fetch...😭 Internal server error.....');
+                }
+            return res.json();
+        })
+        .then(data=>{
+            setBlogs(data);
+            setisPending(false);
+            setError(null);
+        })
+            .catch(err=>{
+                setisPending(false);
+               setError(err.message);
+            })
+        }, 1000);   
+    }, []);
     return ( 
         <div className="Home">
-            
-            {blogs.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading.....</div> }
+            {blogs && <BlogList blogs= {blogs} title= "All Blogs!"/>}
         </div>
 
      );
